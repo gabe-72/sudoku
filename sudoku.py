589b4a952063d42507b8ebcf6eb2e62362839351
gabe-72 <96758638+gabe-72@users.noreply.github.com>
2025-08-25 20:03:09 -0400

    fix: added validity checking

--- sudoku.py
+++ sudoku.py
@@ -51,7 +51,44 @@ class Sudoku:
         Returns:
             bool: True if valid, False otherwise
         """
-        pass
+        ones = np.ones((self._GRID_LENGTH, 1), dtype=np.uint16)
+
+        # Check dimensions
+        if self.SUDOKU_GRID.shape != self._GRID_SHAPE:
+            # Should never happen
+            raise Exception(f'Sudoku Grid shape invalid {self.SUDOKU_GRID.shape}')
+
+        # Check rows
+        row_sum = self.SUDOKU_GRID @ ones
+        if not np.all(row_sum == self._VALID_SUM):
+            return False
+
+        # Check columns
+        col_sum = self.SUDOKU_GRID.T @ ones
+        if not np.all(col_sum == self._VALID_SUM):
+            return False
+
+        # Check each sub square
+        ones = ones.reshape(self._SUBGRID_SHAPE)
+
+        # Loop the rows, stepping by the length of the subgrid
+        for row_start in range(0, self._GRID_LENGTH, self._SUBGRID_LENGTH):
+            # Compute the end of the subgrid row
+            row_end = row_start + self._SUBGRID_LENGTH
+
+            # Loop through the columns now
+            for col_start in range(0, self._GRID_LENGTH, self._SUBGRID_LENGTH):
+                # Compute the end of the subgrid column
+                col_end = col_start + self._SUBGRID_LENGTH
+
+                # Get a view of the subgrid
+                subgrid = self.SUDOKU_GRID[row_start:row_end, col_start:col_end]
+
+                # Check if the subgrid contains all the numbers
+                if np.vdot(subgrid, ones) != self._VALID_SUM:
+                    return False
+
+        return True
 
     def shuffle(self) -> None:
         """
