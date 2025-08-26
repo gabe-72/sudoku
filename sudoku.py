3c53b6939eb9c0a8c0693610f461b84635a4ab66
gabe-72 <96758638+gabe-72@users.noreply.github.com>
2025-08-25 21:49:58 -0400

    fix: optimized subgrid check

--- sudoku.py
+++ sudoku.py
@@ -2,6 +2,8 @@ import numpy as np
 
 class Sudoku:
     # Binary encoding of the numbers
+    # this allows us to easily compute whether a row has duplicates or not by simply adding up the rows
+    # as long as the number of elements is equal to the number of unique numbers
     _1 = 0b000000001
     _2 = 0b000000010
     _3 = 0b000000100
@@ -44,9 +46,15 @@ class Sudoku:
         if (shuffled):
             self.shuffle()
 
-    def is_valid(self) -> bool:
+    def _is_valid(self) -> bool:
         """
         Checks for validity of the sudoku grid.
+
+        Validity checks:
+        - dimension check for the grid
+        - each row has the numbers 1-9
+        - each column has the numbers 1-9
+        - each subgrid has the numbers 1-9
 
         Returns:
             bool: True if valid, False otherwise
@@ -58,36 +66,33 @@ class Sudoku:
             # Should never happen
             raise Exception(f'Sudoku Grid shape invalid {self.SUDOKU_GRID.shape}')
 
-        # Check rows
+        # Check rows, see if every row contains all the numbers
+        # equivalent to summing the rows, but for a standard grid (9x9), this is faster
         row_sum = self.SUDOKU_GRID @ ones
         if not np.all(row_sum == self._VALID_SUM):
             return False
 
-        # Check columns
+        # Check columns, see if every column contains all the numbers
         col_sum = self.SUDOKU_GRID.T @ ones
         if not np.all(col_sum == self._VALID_SUM):
             return False
 
-        # Check each sub square
-        ones = ones.reshape(self._SUBGRID_SHAPE)
+        # Check the subgrids
+        # Reshape the grid into (subgrid rows, num of rows in each subgrid, subgrid cols, num of cols in each subgrid)
+        # indicies 0 -> subgrid row; 1 -> the row number in the subgrid; 2 -> subgrid col; 3 -> the col number in the subgrid
+        reshaped_grid = self.SUDOKU_GRID.reshape(
+            self._GRID_LENGTH // self._SUBGRID_LENGTH, self._SUBGRID_LENGTH,
+            self._GRID_LENGTH // self._SUBGRID_LENGTH, self._SUBGRID_LENGTH
+        )
 
-        # Loop the rows, stepping by the length of the subgrid
-        for row_start in range(0, self._GRID_LENGTH, self._SUBGRID_LENGTH):
-            # Compute the end of the subgrid row
-            row_end = row_start + self._SUBGRID_LENGTH
+        # Sum the rows in the subgrids and the columns in the subgrid
+        subgrid_sums = reshaped_grid.sum(axis=(1, 3))
+        print(subgrid_sums)
 
-            # Loop through the columns now
-            for col_start in range(0, self._GRID_LENGTH, self._SUBGRID_LENGTH):
-                # Compute the end of the subgrid column
-                col_end = col_start + self._SUBGRID_LENGTH
+        if not np.all(subgrid_sums == self._VALID_SUM):
+            return False
 
-                # Get a view of the subgrid
-                subgrid = self.SUDOKU_GRID[row_start:row_end, col_start:col_end]
-
-                # Check if the subgrid contains all the numbers
-                if np.vdot(subgrid, ones) != self._VALID_SUM:
-                    return False
-
+        # All checks passed
         return True
 
     def shuffle(self) -> None:
@@ -105,3 +110,4 @@ if __name__ == '__main__':
     sudoku = Sudoku(False)
 
     print(sudoku.SUDOKU_GRID)
+    print(sudoku._is_valid())
